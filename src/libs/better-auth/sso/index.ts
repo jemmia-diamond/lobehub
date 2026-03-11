7a03067a95b14f367166f1e7d5f935b7c74919ba
Your Name <your.email@example.com>
2026-03-11 19:24:34 +0700

    fix workflow

--- src/libs/better-auth/sso/index.ts
+++ src/libs/better-auth/sso/index.ts
@@ -92,7 +92,7 @@ export const initBetterAuthSSOProviders = () => {
     const definition = providerRegistry.get(rawProvider);
 
     if (!definition) {
-      throw new Error(`[Better-Auth] Unknown SSO provider testtt: ${rawProvider}`);
+      throw new Error(`[Better-Auth] Unknown SSO provider: ${rawProvider}`);
     }
 
     /**
